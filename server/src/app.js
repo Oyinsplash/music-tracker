8a10fe8ad697dbffaefc9e6666aba8e705c43fb8
Oyinsplash <alabi.oyinkansola14@outlook.com>
2020-12-17 01:34:37 +0100

    Initialized the vuejs app using the vue cli

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/status", (req, res) => {
+    res.send({
+        message: "Yaay!!! world"
+    })
+})
+
+app.listen(process.env.PORT || 8081);
